2c14b28e8a666dc3f481c3b2b391a694d22029c4
Ankush-Arora-0 <ankushsatnam@gmail.com>
2023-07-13 17:01:32 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import './db/conn.js';
-import router  from './router/auth.js';
+import './server/db/conn.js';
+import router  from './server/router/auth.js';
 import Razorpay from 'razorpay'
-import PaymentRouter from './router/Paymentroute.js';
+import PaymentRouter from './server/router/Paymentroute.js';
 import BodyParser from 'body-parser';
 import path from 'path';
 import {fileURLToPath} from 'url';
@@ -20,10 +20,10 @@ app.use(router);
 app.use(BodyParser.json());
 app.use(express.json());
 app.use('/api',PaymentRouter)
-app.use(express.static(path.join(__dirname,"../client/build")))
+app.use(express.static(path.join(__dirname,"./client/build")))
 
 app.use("*",function(req,res){
-    res.sendFile(path.join(__dirname,"../client/build/index.html"));
+    res.sendFile(path.join(__dirname,"./client/build/index.html"));
 })
 export const instance = new Razorpay({
     key_id:process.env.KEY_ID,
